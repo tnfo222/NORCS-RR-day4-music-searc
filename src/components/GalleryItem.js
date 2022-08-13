32fa4030e8541b389da47c51d5fd4e6772fc8ee0
tnfo222 <tayforbes92@hotmail.com>
2022-08-13 16:34:18 -0400

    Search with Router

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function GalleryItem(props){
     let [view, setView] = useState(false)
@@ -36,21 +37,24 @@ function GalleryItem(props){
 
     const detailView = () => {
         return (
-            <div id="detailview" style={detailStyle}>
+            <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
-                <p>
-                    <img src={props.item.artworkUrl100} alt="" width="100px" />
-                    <br />
-                    <a href={props.item.trackViewUrl} target="_blank">
-                        <button>Preview Song</button>
-                    </a>
-                </p>
             </div>
         )
     }
+    
     return (
         <div onClick={() => setView(!view)} style={{'display': 'inline-block'}}>
             <p>One Gallery Item</p>
